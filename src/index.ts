88d57709b30e2fd13040f7645ffaf5c90eeaa195
noreederek <mayorovyuri7@gmail.com>
2023-05-29 17:03:40 +0300

    CSS, Images and Font Loaders

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import * as _ from 'lodash';
 import $ from "jquery";
+import './style.css';
+// import Icon from './icon.png';
 
 function component() {
   const element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello', 'webpadckd'], ' ');
+  element.innerHTML = _.join(['Appended', 'with', 'Typescript'], ' ');
+  element.classList.add('hello');
+
+  // const myIcon = new Image();
+  // myIcon.src = Icon;
+
+  // element.appendChild(myIcon);
 
   return element;
 }
 
-
-$('<footer>Appended with JQ</footer>').appendTo(document.body);
+$('<div>Appended with JQuery</div>').appendTo(document.body);
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,18 @@ module.exports = {
         exclude: /node_modules/,
       },
       {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
         test: /\.ejs$/,
         use: [
           {
